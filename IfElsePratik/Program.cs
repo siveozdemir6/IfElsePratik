cb578fd512024fb1bbb48c3faa4302d49f4c965d
siveozdemir6 <siveozdemir6@gmail.com>
2025-05-08 12:39:12 +0300

    Add project files.

---
+++ IfElsePratik/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.Design;
+
+Console.Write("Lütfen bir sayı giriniz : "); 
+int sayı = Convert.ToInt32(Console.ReadLine()); //Kullanıcıdan aldığımız sayıyı okuyoruz.
+if (sayı > 10)   //Girilen sayının 10'dan büyük, küçük ya da eşit olup olmadığını kontrol eden if, else if ve else kullanımı.
+{ Console.WriteLine("Girilen sayı 10'dan büyüktür."); }
+else if (sayı < 10) 
+{ Console.WriteLine("Girilen sayı 10'dan küçüktür."); }
+else 
+{ Console.WriteLine("Girilen sayı 10'a eşittir."); }
+
+if (sayı % 2 == 0) //Girilen sayının tek ya da çift olup olmadığını kontrol eden if ve else kullanımı.
+{ Console.WriteLine("Girilen sayı çifttir."); }
+else
+{ Console.WriteLine("Girilen sayı tektir."); }
+
